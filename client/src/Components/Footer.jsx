1d96c5d6cc3320fe82e0a587c3609ea7a007cbfe
DaniloWaffis <danilowaffis@gmail.com>
2024-05-06 10:28:39 +0100

    create and add the footer component

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitterX,
+  BsGithub,
+  BsLinkedin,
+} from "react-icons/bs";
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex-row md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap txt-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Isis's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About"></Footer.Title>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.100jsprojects.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  100 JS Projects
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Isis'sBlog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow me"></Footer.Title>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/WaffoKom/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Telegram
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal"></Footer.Title>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/WaffoKom/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Terms &amp; Conditions
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Isis's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="https://github.com/WaffoKom/" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsTwitterX} />
+            <Footer.Icon href="#" icon={BsLinkedin} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
