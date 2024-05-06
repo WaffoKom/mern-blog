8fef13befe20b4e20935a9022ddb69917d74589e
DaniloWaffis <danilowaffis@gmail.com>
2024-05-06 18:11:46 +0100

    create signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import { userModel } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export async function signup(req, res) {
   const { username, email, password } = req.body;
@@ -28,6 +31,43 @@ export async function signup(req, res) {
     return res.status(500).send({
       success: false,
       message: "Erreur interne",
+      error: error.message,
+    });
+  }
+}
+
+export async function signin(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      res.status(400).send({ message: " user is not registered !" });
+      return;
+    }
+    // const validPassword = await bcrypt.compare(password, user.password);
+    // if (!validPassword) {
+    //   return res.status(400).send({ message: "password is incorrect !" });
+    // }
+    const token = jwt.sign(
+      { email: user.email, userId: user._id },
+      process.env.KEY,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .send({ message: "Login Successfull", user, token });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Intern Error",
       error: error.message,
     });
   }
