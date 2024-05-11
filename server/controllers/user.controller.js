23425d9f3f75dd9defa97634274480c51e439132
DaniloWaffis <danilowaffis@gmail.com>
2024-05-11 20:34:50 +0100

    add delete api route and controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -56,3 +56,23 @@ export async function updateUser(req, res) {
       .send({ message: "Intern error", error: error.message });
   }
 }
+
+export const deleteUser = async (req, res) => {
+  // if (req.user.id !== req.params.userId) {
+  //   return res.status(403).json("You are not allowed to delete this user");
+  // }
+
+  try {
+    const deletedUser = await userModel.findByIdAndDelete(req.params.userId);
+    if (deletedUser) {
+      res.status(200).json(deletedUser);
+    } else {
+      res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+  } catch (error) {
+    // Gérez les erreurs de manière appropriée
+    res
+      .status(500)
+      .json({ message: "Erreur interne du serveur", error: error.message });
+  }
+};
