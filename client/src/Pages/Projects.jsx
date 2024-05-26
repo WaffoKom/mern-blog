2330951d41d5030a65dcb85e77f97a894bf946ed
DaniloWaffis <danilowaffis@gmail.com>
2024-05-26 18:55:31 +0100

    complete the project page

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,6 +1,13 @@
-import CallToAction from "../Components/CallToAction.jsx";
+import { CallToAction } from "../Components/CallToAction.jsx";
+import React, { useEffect, useRef } from "react";
 
 export default function Projects() {
+  const customButtonRef = useRef(null);
+  useEffect(() => {
+    if (customButtonRef.current) {
+      console.log(customButtonRef.current);
+    }
+  }, []);
   return (
     <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
       <h1 className="text-3xl font-semibold">Projects</h1>
@@ -8,7 +15,9 @@ export default function Projects() {
         Build fun and engaging projects while learning HTML, CSS, and
         JavaScript!
       </p>
-      {/* <CallToAction /> */}
+      <>
+        <CallToAction ref={customButtonRef} />
+      </>
     </div>
   );
 }
