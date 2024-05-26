7d71a36273f5ee00b0d9b48e30cd1fb80d0dddea
DaniloWaffis <danilowaffis@gmail.com>
2024-05-26 19:50:27 +0100

    correct found postId

--- client/src/Components/DashComments.jsx
+++ client/src/Components/DashComments.jsx
@@ -1,34 +1,28 @@
-import { Table } from "flowbite-react";
-import React, { useEffect, useState } from "react";
+import { Modal, Table, Button } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Button, Modal } from "flowbite-react";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { FaCheck, FaTimes } from "react-icons/fa";
 
 export default function DashComments() {
   const { currentUser } = useSelector((state) => state.user);
   const [comments, setComments] = useState([]);
-  const [showMore, setshowMore] = useState(true);
-  const [showModal, setshowModal] = useState(false);
+  const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const [commentIdToDelete, setCommentIdToDelete] = useState("");
-  console.log(comments);
   useEffect(() => {
     const fetchComments = async () => {
       try {
-        const res = await fetch("/api/comment/getComments/", {
-          method: "GET",
-        });
+        const res = await fetch(`/api/comment/getComments`);
         const data = await res.json();
-        console.log(data);
         if (res.ok) {
           setComments(data.comments);
-          if (data.comments.length < 5) {
-            setshowMore(false);
+          if (data.comments.length < 9) {
+            setShowMore(false);
           }
         }
       } catch (error) {
-        // console.log(error.message);
-        throw { message: "Intern Error", error: error.message };
+        console.log(error.message);
       }
     };
     if (currentUser.isAdmin) {
@@ -37,24 +31,25 @@ export default function DashComments() {
   }, [currentUser._id]);
 
   const handleShowMore = async () => {
-    const startIndex = s.length;
+    const startIndex = comments.length;
     try {
       const res = await fetch(
         `/api/comment/getComments?startIndex=${startIndex}`
       );
-      const data = res.json();
+      const data = await res.json();
       if (res.ok) {
-        setUsers((prev) => [...prev, ...data.comments]);
+        setComments((prev) => [...prev, ...data.comments]);
         if (data.comments.length < 9) {
-          setshowMore(false);
+          setShowMore(false);
         }
       }
     } catch (error) {
-      console.error(error);
+      console.log(error.message);
     }
   };
+
   const handleDeleteComment = async () => {
-    setshowModal(false);
+    setShowModal(false);
     try {
       const res = await fetch(
         `/api/comment/deleteComment/${commentIdToDelete}`,
@@ -62,26 +57,29 @@ export default function DashComments() {
           method: "DELETE",
         }
       );
-      const data = res.json();
-      if (!res.ok) {
+      const data = await res.json();
+      if (res.ok) {
+        setComments((prev) =>
+          prev.filter((comment) => comment._id !== commentIdToDelete)
+        );
+        setShowModal(false);
+      } else {
         console.log(data.message);
       }
-      setComments((prev) =>
-        prev.filter((comment) => comment._id !== commentIdToDelete)
-      );
     } catch (error) {
-      console.error(error.message);
+      console.log(error.message);
     }
   };
+
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300">
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && comments.length > 0 ? (
         <>
-          <Table hoverable className="shadow-md" as="div">
+          <Table hoverable className="shadow-md">
             <Table.Head>
               <Table.HeadCell>Date updated</Table.HeadCell>
-              <Table.HeadCell> Comment likes</Table.HeadCell>
-              <Table.HeadCell>Numbers of likes</Table.HeadCell>
+              <Table.HeadCell>Comment content</Table.HeadCell>
+              <Table.HeadCell>Number of likes</Table.HeadCell>
               <Table.HeadCell>PostId</Table.HeadCell>
               <Table.HeadCell>UserId</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
@@ -93,18 +91,16 @@ export default function DashComments() {
                     {new Date(comment.updatedAt).toLocaleDateString()}
                   </Table.Cell>
                   <Table.Cell>{comment.content}</Table.Cell>
-                  <Table.Cell>{comment.likes}</Table.Cell>
                   <Table.Cell>{comment.numberOfLikes}</Table.Cell>
-                  <Table.Cell>{comment.PostId}</Table.Cell>
+                  <Table.Cell>{comment.postId}</Table.Cell>
                   <Table.Cell>{comment.userId}</Table.Cell>
-
                   <Table.Cell>
                     <span
-                      className="font-medium text-red-500 hover:underline cursor-pointer"
                       onClick={() => {
-                        setshowModal(true);
+                        setShowModal(true);
                         setCommentIdToDelete(comment._id);
                       }}
+                      className="font-medium text-red-500 hover:underline cursor-pointer"
                     >
                       Delete
                     </span>
@@ -115,35 +111,34 @@ export default function DashComments() {
           </Table>
           {showMore && (
             <button
-              className="w-full text-teal-500 self-center text-sm py-7"
               onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
             >
               Show more
             </button>
           )}
         </>
       ) : (
-        <p>You have not comments yet !</p>
+        <p>You have no comments yet!</p>
       )}
       <Modal
         show={showModal}
-        onClose={() => setshowModal(false)}
+        onClose={() => setShowModal(false)}
         popup
         size="md"
-        as="div"
       >
         <Modal.Header />
         <Modal.Body>
           <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400  dark:text-gray-200 mb-4 mx-auto" />
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
             <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
-              Are you sure that you want to delete this comment?
+              Are you sure you want to delete this comment?
             </h3>
             <div className="flex justify-center gap-4">
               <Button color="failure" onClick={handleDeleteComment}>
                 Yes, I'm sure
               </Button>
-              <Button color="gray" onClick={() => setshowModal(false)}>
+              <Button color="gray" onClick={() => setShowModal(false)}>
                 No, cancel
               </Button>
             </div>
