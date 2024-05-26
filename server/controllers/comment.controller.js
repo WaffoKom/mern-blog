1cccc1f7f05348f0dd61e718022a071cb390927d
DaniloWaffis <danilowaffis@gmail.com>
2024-05-26 19:16:20 +0100

    like only one time

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -64,20 +64,28 @@ export const getComments = async (req, res) => {
   }
 };
 
-export const likeComment = async (req, res, next) => {
+export const likeComment = async (req, res) => {
   try {
     const comment = await commentModel.findById(req.params.commentId);
     if (!comment) {
       return res.status(404).json("Comment not found");
     }
+
     const userIndex = comment.likes.indexOf(req.user.id);
+
     if (userIndex === -1) {
-      comment.numberOfLikes += 1;
       comment.likes.push(req.user.id);
     } else {
-      comment.numberOfLikes += 1;
       comment.likes.splice(userIndex, 1);
     }
+
+    // Update number of likes only if the user liked/unliked the comment
+    if (userIndex === -1) {
+      comment.numberOfLikes += 1;
+    } else if (comment.likes.length > 0) {
+      comment.numberOfLikes -= 1;
+    }
+
     await comment.save();
     res.status(200).json(comment);
   } catch (error) {
