9fc8047696f18833a54a1e20d4fd9fbf8a4c4762
WaffoKom <danilowaffis@gmail.com>
2024-06-09 00:15:38 +0200

    Revert "correct the link road  sign-in to signin"
    This reverts commit 5f43bc959be274c31cd6d7159fbfcc79b9335ed7.

--- client/src/Components/CommentSection.jsx
+++ client/src/Components/CommentSection.jsx
@@ -59,7 +59,7 @@ export default function CommentSection({ postId }) {
   const handleLike = async (commentId) => {
     try {
       if (!currentUser) {
-        navigate('/signin');
+        navigate('/sign-in');
         return;
       }
       const res = await fetch(`/api/comment/likeComment/${commentId}`, {
@@ -96,7 +96,7 @@ export default function CommentSection({ postId }) {
     setShowModal(false);
     try {
       if (!currentUser) {
-        navigate('/signin');
+        navigate('/sign-in');
         return;
       }
       const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
@@ -130,7 +130,7 @@ export default function CommentSection({ postId }) {
       ) : (
         <div className='text-sm text-teal-500 my-5 flex gap-1'>
           You must be signed in to comment.
-          <Link className='text-blue-500 hover:underline' to={'/signin'}>
+          <Link className='text-blue-500 hover:underline' to={'/sign-in'}>
             Sign In
           </Link>
         </div>
