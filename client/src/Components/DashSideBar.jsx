4d656c50006c5b7d042992c199e50853959d7f4e
DaniloWaffis <danilowaffis@gmail.com>
2024-07-08 00:35:13 +0100

    unused code

--- client/src/Components/DashSideBar.jsx
+++ client/src/Components/DashSideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Sidebar } from "flowbite-react";
 import {
   HiAnnotation,
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Navbar, TextInput, Dropdown, Avatar } from "flowbite-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
