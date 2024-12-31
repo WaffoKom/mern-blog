0911fca8cdddfab71dbca9ab4a972b5dd2bd50d7
DaniloWaffis <danilowaffis@gmail.com>
2025-01-01 00:26:52 +0100

    update config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://mern-blog-api-red.vercel.app",
+        changeOrigin: true,
         secure: true,
       },
     },
