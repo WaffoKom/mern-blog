28d258b348f8dc130c498ebe0d3a20509e4603d8
DaniloWaffis <danilowaffis@gmail.com>
2025-03-09 13:02:29 +0100

    ajouter le chargement des variables d'environnement dans la configuration de Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import process from "node:process";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_HOST,
-        changeOrigin: true,
-        secure: true,
+export default defineConfig(({ mode }) => {
+  // Charger les variables d'environnement en fonction du mode
+  const env = loadEnv(mode, process.cwd());
+
+  console.log("Loaded env:", env); // Vérifie que les variables sont bien chargées
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_HOST, // Utilisation de la variable d'environnement
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
